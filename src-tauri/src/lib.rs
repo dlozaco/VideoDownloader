fda1123d631ad8dc393e70b46edf1548b4ddb0bd
David Lozano <dlozaco975@gmail.com>
2026-04-26 16:20:52 +0200

    chore: now execute on linux

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -200,6 +200,10 @@ async fn descargar_video_cmd(
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    #[cfg(target_os = "linux")]
+    {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
